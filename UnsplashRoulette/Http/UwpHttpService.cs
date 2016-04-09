a613f547b9870cc45bd85de5a8a4ce740b2ec3d2
James Wright <james@jamesswright.co.uk>
2016-04-09 16:53:51 +0100

    WIP HTTP

---
+++ UnsplashRoulette/Http/UwpHttpService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Web.Http.Headers;
+using Windows.Web.Http;
+using Windows.Storage.Streams;
+
+namespace UnsplashRoulette.Http
+{
+    class UwpHttpService : HttpService
+    {
+        HttpClient httpClient;
+        HttpRequestHeaderCollection headers;
+
+        UwpHttpClient()
+        {
+            this.httpClient = new Windows.Web.Http.HttpClient();
+            this.headers = httpClient.DefaultRequestHeaders;
+        }
+
+        public async override Task<IInputStream> GetAsync(string url, Dictionary<string, string> headers = null)
+        {
+            if (headers != null)
+            {
+                AddHeaders(headers);
+            }
+
+            HttpResponseMessage response = await this.httpClient.GetAsync(new Uri(url));
+            return await response.Content.ReadAsInputStreamAsync();
+        }
+
+        private void AddHeaders(Dictionary<string, string> headers)
+        {
+            foreach (KeyValuePair<string, string> header in headers)
+            {
+                this.headers.Add(header.Key, header.Value);
+            }
+        }
+    }
+}
---
+++ UnsplashRoulette/Photos/IPhotoService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnsplashRoulette.Photos
+{
+    interface IPhotoService
+    {
+        Photo GetRandomPhoto(int width, int height);
+    }
+}
